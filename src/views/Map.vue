60e95e349dd10b2fac03c27e2e318ee3c178061c
Shintaro Itamoto <itashin0501@gmail.com>
2019-06-13 22:35:05 +0900

    Map.vueにgoogleMapを表示できるように修正

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
     <h2>MAP</h2>
-    <div id = "map">
-      <button @click="">範囲指定</button> 
-      <button @click="">設定</button>
-    </div>
+    <div id="map" style="height:500px;">aaa</div>
     <br>
     <a @click="create()">ﾎﾟｲﾝﾄ作成画面に遷移（仮）</a>
     <br>
@@ -13,26 +10,26 @@
 </template>
 
 <script>
-import Point from '../model/Point'
+
+// import Point from '../model/Point'
 export default {
   data () {
     return {
     }
   },
-  /*単なる地図の表示のみ
+  // 単なる地図の表示のみ
   mounted() {
-    const MyLatLng = new google.maps.LatLng(35.6811673, 139.7670516);
-    const options = {
-      zoom: 15,      //地図の縮尺値
-      center: MyLatLng,    //地図の中心座標
-      mapTypeId: 'roadmap'   //地図の種類
+    var MyLatLng = new google.maps.LatLng(35.6811673, 139.7670516);
+    var Options = {
+    zoom: 15,      //地図の縮尺値
+    center: MyLatLng,    //地図の中心座標
+    mapTypeId: 'roadmap'   //地図の種類
     };
-    const map = new google.maps.Map(document.getElementById('map'), Options);
-    },
-    */
+    var map = new google.maps.Map(document.getElementById('map'), Options);
+  },
   created() {
-    const point = new Point(11, 43);
-    console.log(point.getPos());
+    // const point = new Point(11, 43);
+    // console.log(point.getPos());
   },
   methods: {
     create(){
