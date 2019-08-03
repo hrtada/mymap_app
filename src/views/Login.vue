d20082a5143552d5c204d7772dbe3cfb0f877f46
Shintaro Itamoto <itashin0501@gmail.com>
2019-08-03 23:55:48 +0900

    googleアカウントでのログイン機能を仮実装

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,14 +7,52 @@
 </template>
 
 <script>
+import * as firebase from "firebase";
 import UserInfo from './UserInfo'
 export default {
   data () {
     return {
-      screen: "login"
+      screen: "login",
+      provider: null
     }
   },
   created() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyAGOB6BSOtrSwkcAr8uA4HeIlZk29AdYsU",
+      authDomain: "mtfirebaseproject-64e86.firebaseapp.com",
+      databaseURL: "https://mtfirebaseproject-64e86.firebaseio.com",
+      projectId: "mtfirebaseproject-64e86",
+      storageBucket: "mtfirebaseproject-64e86.appspot.com",
+      messagingSenderId: "969784300164",
+      appId: "1:969784300164:web:fddc0f90fc2a0916"
+    };
+    // Initialize
+    const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+    firebaseApp.auth().onAuthStateChanged((authUser) => {
+      if (authUser) {
+        console.log(authUser);
+      }
+    });
+
+    this.provider = new firebase.auth.GoogleAuthProvider();
+    this.provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
+    firebaseApp
+      .auth()
+      .signInWithPopup(this.provider)
+      .then(result => {
+        // const token = result.credential.accessToken
+        console.log(result.user);
+        this.$router.push({ path: "/" });
+      })
+      .catch(function(error) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // const email = error.email
+        // const credential = error.credential
+        console.log(errorCode + " : " + errorMessage);
+      });
   },
   components: { UserInfo },
   methods: {
