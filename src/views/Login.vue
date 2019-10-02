9c66d4c2cb367dd78c2e8514a2ab0ef712b5ab20
Shintaro Itamoto <itashin0501@gmail.com>
2019-10-03 01:30:57 +0900

    postでglitchのexpressでパラメータ取得するサンプル

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,104 +1,114 @@
 <template>
-<div id="app">
-<body class="layout-default">
-  <section class="hero is-fullheight is-medium is-primary">
+  <div id="app">
+
+    <body class="layout-default">
+      <section class="hero is-fullheight is-medium is-primary">
         <div class="hero-body">
           <div class="container">
             <div class="columns is-centered">
               <article class="card is-rounded">
                 <div class="card-content">
                   <h1 class="is-size-4 has-text-weight-bold"> Login</h1>
-                  <button @click="login()"><img src="../assets/btn_google_signin.png" alt="ログインする"></button>
+                  <button @click="login()"><img
+                      src="../assets/btn_google_signin.png"
+                      alt="ログインする"
+                    ></button>
+                  <button @click="requestTest('testuser')">test</button>
                 </div>
               </article>
             </div>
           </div>
-    </div>
-  </section>
-</body> 
-</div>
+        </div>
+      </section>
+    </body>
+  </div>
 </template>
 
 <script>
 /*eslint-disable no-console */
-import 'bulma/css/bulma.css';//CSSフレームワーク
-import  firebaseApp from '../firebase';
+import "bulma/css/bulma.css"; //CSSフレームワーク
+import firebaseApp from "../firebase";
 import * as firebase from "firebase/app";
 //import MymapLabelService from '../database/firestore/service/MymapLabelService'
-import MymapLabelServiceMysql from '../database/firestore/service/MymapLabelserviceMysql'
+import MymapLabelServiceMysql from "../database/firestore/service/MymapLabelserviceMysql";
 
 //let db = firebaseApp.firestore()
 
 export default {
-  data () {
-    return {
-    }
+  data() {
+    return {};
   },
 
   methods: {
+    login() {
+      firebaseApp.auth().onAuthStateChanged(authUser => {
+        if (authUser) {
+          console.log(authUser);
+        }
+      });
 
-    login () {
-    firebaseApp.auth().onAuthStateChanged((authUser) => {
-      if (authUser) {
-        console.log(authUser);
-      }
-    });
+      this.provider = new firebase.auth.GoogleAuthProvider();
+      this.provider.addScope(
+        "https://www.googleapis.com/auth/contacts.readonly"
+      );
 
-    this.provider = new firebase.auth.GoogleAuthProvider();
-    this.provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+      firebaseApp
+        .auth()
+        .signInWithPopup(this.provider)
+        .then(result => {
+          // const token = result.credential.accessToken
+          //ログインユーザーのIDと名前を取得
+          const user = result.user;
+          const userUid = user.uid;
+          const userName = user.displayName;
+          this.$store.commit("setuserUid", { userUid: userUid });
+          this.$store.commit("setuserName", { userName: userName });
+          console.log(this.$store.state.userUid);
 
-    firebaseApp 
-      .auth()
-      .signInWithPopup(this.provider)
-      .then(result => {
-        // const token = result.credential.accessToken
-        //ログインユーザーのIDと名前を取得
-        const user = result.user;
-        const userUid = user.uid;
-        const userName = user.displayName;
-        this.$store.commit('setuserUid',{userUid:userUid});
-        this.$store.commit('setuserName',{userName:userName});
-        console.log(this.$store.state.userUid);
-   
-        //ラベル情報を取得し、storeに渡す
-/*         const mymapLabelService = new MymapLabelService();
+          //ラベル情報を取得し、storeに渡す
+          /*         const mymapLabelService = new MymapLabelService();
         const label = mymapLabelService.getLabel(userUid);
         this.$store.commit('setlabel',{label: label}); */
 
-        //ユーザー情報を渡す
-        //new MymapUserService.setUserId();
-        const request = require('request');
-          const option = {
-              url: 'https://airy-quicksand.glitch.me/',
-              method: 'POST',
-              body: {
-                'userId': userUid
-              }
-          }
-          request.post(option, function(error, response, body){});      
+          //ユーザー情報を渡す
+          //new MymapUserService.setUserId();
+          this.requestTest(userUid);
 
-        //ラベル情報を取得し、storeに渡す(MySQL)
-        const mymapLabelService = new MymapLabelServiceMysql();
-        const label = mymapLabelService.getLabel();
-        this.$store.commit('setlabel',{label: label});
-        console.log(label);
+          //ラベル情報を取得し、storeに渡す(MySQL)
+          const mymapLabelService = new MymapLabelServiceMysql();
+          const label = mymapLabelService.getLabel();
+          this.$store.commit("setlabel", { label: label });
+          console.log(label);
 
-        //map画面に移動
-        this.$router.push({ path: "/map" });
+          //map画面に移動
+          this.$router.push({ path: "/map" });
+        })
+        .catch(function(error) {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // const email = error.email
+          // const credential = error.credential
+          console.log(errorCode + " : " + errorMessage);
+        });
+    },
 
-      })
-      .catch(function(error) {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // const email = error.email
-        // const credential = error.credential
-        console.log(errorCode + " : " + errorMessage);
+    requestTest(userUid) {
+      const request = require("request");
+      const options = {
+        url: "https://airy-quicksand.glitch.me/",
+        method: "POST",
+        timeout: 5000,
+        form: { userUid: userUid }
+      };
+      request(options, (error, response, body) => {
+        console.log(error);
+        console.log(response);
+        console.log(body);
       });
     }
   }
-}
+};
 </script>
 
 <style>
-
 </style>
